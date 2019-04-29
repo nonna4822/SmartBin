8a30d762f43068701e0da077e2a805c9aef47c55
saalimon <niti08@gmail.com>
2019-04-29 17:57:58 +0700

    git add

--- PIR_plus_Servo/Bin/Bin/main.c
+++ PIR_plus_Servo/Bin/Bin/main.c
@@ -5,22 +5,23 @@
  * Author : elesr
  */ 
 
-#ifndef F_CPU
-#define F_CPU 8000000UL
-#endif
+#define F_CPU 1000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 
 int main(void)
 {
-	DDRB |= (1<<PB1);	/* Make OC1A pin as output */
+	DDRB |= (1<<PORTB1);	/* Make OC1A pin as output */
 	TCNT1 = 0;		/* Set timer1 count zero */
 	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
+	//TCCR1A|=(1<<COM1A1)|(1<<WGM11);        //NON Inverted PWM
+	//TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11); //PRESCALER=8 MODE 14(FAST PWM)
+
 	
 	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
 	TCCR1A = (1<<WGM11)|(1<<COM1A1);
-	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS11);
 	
 	/* Replace with your application code */
 	while (1)
@@ -28,10 +29,10 @@ int main(void)
 		OCR1A = 0;
 		if(PINB & (1<<PORTB) ){
 			if(PIND & (1<<PIND2)){ //close bin
-				OCR1A = 0;
+				OCR1A = 175;
 				_delay_ms(100);
 			}else {
-				OCR1A = 65;	//open bin
+				OCR1A = 300;	//open bin
 				_delay_ms(100);
 			}
 			
